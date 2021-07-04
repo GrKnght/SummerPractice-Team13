6953bf1caee7c119ecc03b9bd0592aa2797becd2
Iskandar <iskandar200213@gmail.com>
2021-07-04 17:22:22 +0300

    featureWhenButtonIsClicked

---
+++ app/src/main/java/summerpractice/team13/guessthedrawing/mvp/views/AnswerCheckView.kt
@@ -0,0 +1,10 @@
+package summerpractice.team13.guessthedrawing.mvp.views
+
+import android.content.Context
+
+interface AnswerCheckView {
+
+    fun showTrueIcon(context: Context)
+    fun showFalseIcon(context: Context)
+
+}
--- app/src/main/java/summerpractice/team13/guessthedrawing/ui/play_screen/HomeFragment.kt
+++ app/src/main/java/summerpractice/team13/guessthedrawing/ui/play_screen/HomeFragment.kt
@@ -1,31 +1,74 @@
 package summerpractice.team13.guessthedrawing.ui.play_screen
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import summerpractice.team13.guessthedrawing.R
+import summerpractice.team13.guessthedrawing.mvp.presenters.AnswerCheckPresenter
+import summerpractice.team13.guessthedrawing.mvp.views.AnswerCheckView
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AnswerCheckView {
 
-    private lateinit var homeViewModel: HomeViewModel
+    //private lateinit var homeViewModel: HomeViewModel
+
+    private lateinit var answerCheckPresenter: AnswerCheckPresenter
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        //homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         val root = inflater.inflate(R.layout.fragment_play, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
 
-        homeViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
+        val button: Button = root.findViewById(R.id.TestButton)
+
+        val editText: EditText = root.findViewById(R.id.editTestText)
+
+        answerCheckPresenter = AnswerCheckPresenter(this)
+
+        button.setOnClickListener {
+            context?.applicationContext?.let { it1 ->
+                answerCheckPresenter.chekAnswer(
+                    editText.text.toString(),
+                    it1
+                )
+            }
+
+        }
+
+
         return root
+    }
+
+
+    override fun showTrueIcon(context: Context) {
+        val inflater = layoutInflater
+        val views = inflater.inflate(R.layout.green_toast_item, null)
+        val toast = Toast(context)
+        toast.duration = Toast.LENGTH_SHORT
+        toast.view = views
+
+        toast.show()
+
+    }
+
+    override fun showFalseIcon(context: Context) {
+        val inflater = layoutInflater
+        val views = inflater.inflate(R.layout.red_toast_item, null)
+        val toast = Toast(context)
+        toast.duration = Toast.LENGTH_SHORT
+        toast.view = views
+
+        toast.show()
+
     }
 }
