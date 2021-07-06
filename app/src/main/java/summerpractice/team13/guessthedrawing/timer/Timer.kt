26016f1f502ec7e8d55c2709994e300671054cfb
Iskandar <iskandar200213@gmail.com>
2021-07-06 15:52:00 +0300

    MakingTimer

---
+++ app/src/main/java/summerpractice/team13/guessthedrawing/timer/Timer.kt
@@ -0,0 +1,18 @@
+package summerpractice.team13.guessthedrawing.timer
+
+import android.os.CountDownTimer
+
+class Timer {
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds: Long = 0L
+    private var timerState = TimerState.Stopped
+
+
+    private var secondsRemaining = 0L
+
+
+    fun t(){
+
+    }
+}
---
+++ app/src/main/java/summerpractice/team13/guessthedrawing/timer/TimerState.kt
@@ -0,0 +1,8 @@
+package summerpractice.team13.guessthedrawing.timer
+
+enum class TimerState {
+    Stopped,Paused,Running
+
+
+
+}
